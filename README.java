db116def65829908b620ee0d138e8c0f794f465d
malathi-kpriet <malathikanakaraj14@gmail.com>
2016-07-09 11:17:38 +0530

    finish

---
+++ README.java
@@ -0,0 +1,19 @@
+# positive
+class java 
+{
+public static void main(string args[])throws IOException
+{
+int num=15;
+if(num>0)
+{
+System.out.println("number is positive");
+}
+else 
+{
+System.out.println("number is negative");
+}
+if(num==0)
+{
+System.out.println("number is zero");
+}
+}
